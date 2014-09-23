cb9621566ebe449c191aa96568555c274b7259e1
David Duvenaud <dduvenaud@seas.harvard.edu>
2014-09-23 19:32:35 -0400

    Got first custom molecule-net to automatically build itself

--- src/demo.py
+++ src/demo.py
@@ -13,7 +13,7 @@ import numpy.random as npr
 
 from rdkit import Chem
 
-sys.path.append('../Kayak/')
+sys.path.append('../../Kayak/')
 import kayak
 
 from MolGraph import *
@@ -61,42 +61,62 @@ def test_custom_net():
     print "Numerical derivative:", (second - first) / delta
 
 
-def build_net_from_graph(graph):
+def BuildNetFromGraph(graph, num_hidden_features = [5, 6]):
 
-    num_layers = 7
-    num_dims   = 5
+    # This first version just tries to emulate ECFP.
+    # Different weights on each layer
 
-    for ii in xrange(NUM_TRIALS):
-        probs = npr.rand()
+    num_layers = len(num_hidden_features)
+    num_atom_features = graph.verts[0].nodes[0].shape()[1]
+    num_edge_features = graph.edges[0].nodes[0].shape()[1]
 
-        X = kayak.Inputs(npr.randn(25,num_dims))
+    # Every atom and edge is a separate Kayak Input.
+    # These inputs already live in the graph.
 
-        wts    = []
-        layers = []
-        for jj in xrange(num_layers):
+    W_self = []
+    W_other = []
+    W_edge = []
 
-            U = kayak.Constant(np.zeros((25,num_dims)))
+    for layer in range(num_layers):
 
-            if npr.rand() < probs:
-                W = kayak.Parameter(0.1*npr.randn(num_dims, num_dims))
-                wts.append(W)
-                U = kayak.MatAdd( U, kayak.SoftReLU(kayak.MatMult(X, W)) )
+        num_prev_layer_features = graph.verts[0].nodes[layer].shape()[1]
 
-            for kk in xrange(jj):
-                if npr.rand() < probs:
-                    W = kayak.Parameter(0.1*npr.randn(num_dims, num_dims))
-                    wts.append(W)
-                    U = kayak.MatAdd( U, kayak.SoftReLU(kayak.MatMult(layers[kk], W)) )
+        # Create a Kayak parameter for this layer
+        W_self.append(kayak.Parameter(0.1*npr.randn(num_prev_layer_features, num_hidden_features[layer])))
+        # possible refinement: separate weights for each connection, max-pooled over all permutations
+        W_other.append(kayak.Parameter(0.1*npr.randn(num_prev_layer_features, num_hidden_features[layer])))
+        W_edge.append(kayak.Parameter(0.1*npr.randn(num_edge_features, num_hidden_features[layer])))
 
-            layers.append(U)
+        for v in graph.verts:
+            # Create a Differentiable node N that depends on the corresponding node in the previous layer, its edges,
+            # and its neighbours.
+            mults = [kayak.MatMult(v.nodes[layer], W_self[layer])]
+            for e in v.edges:
+                mults.append(kayak.MatMult( e.nodes[0], W_edge[layer]))
+            for n in v.get_neighbors()[0]:
+                mults.append(kayak.MatMult( n.nodes[layer], W_other[layer]))
 
-        out = kayak.MatSum(layers[-1])
+            # Add the next layer of computation to this node.
+            v.nodes.append(kayak.SoftReLU(kayak.ElemAdd(*mults)))
 
-        out.value(True)
-        for jj, wt in enumerate(wts):
-            diff = kayak.util.checkgrad(wt, out, 1e-4)
-            print diff
-            assert diff < 1e-4
+    # Connect everything to the fixed-size layer using some sort of max
+    penultimate_nodes = [v.nodes[-1] for v in graph.verts]
+    concatenated = kayak.Concatenate( 0, *penultimate_nodes)
+    output_layer = kayak.MatSum( concatenated, 0)
+
+    # Perform a little more computation to get a single number.
+    W_out = kayak.Parameter(0.1*npr.randn(num_hidden_features[-1], 1))
+    output = kayak.MatMult(output_layer, W_out)
+
+    target = kayak.Targets(np.array([[1.23]]));
+    loss = kayak.L2Loss( output, target)
+
+    loss.value(True)
+    print kayak.util.checkgrad(W_self[0], loss, 1e-4)
+
+    weights = W_self + W_other + W_edge + [W_out]
+
+    return loss, weights
 
 
 def BuildGraphFromMolecule(mol):
@@ -108,7 +128,7 @@ def BuildGraphFromMolecule(mol):
     # Iterate over the atoms.
     rd_atoms = {}
     for atom in mol.GetAtoms():
-        rd_atoms[atom.GetIdx()] = Vertex( data=atom_features(atom) )
+        rd_atoms[atom.GetIdx()] = Vertex( nodes = [kayak.Inputs(atom_features(atom)[None,:])] )
         graph.add_vert( rd_atoms[atom.GetIdx()] )
 
     # Iterate over the bonds.
@@ -118,7 +138,7 @@ def BuildGraphFromMolecule(mol):
 
         graph.add_edge( Edge(rd_atoms[atom1.GetIdx()],
                              rd_atoms[atom2.GetIdx()],
-                             data=bond_features(bond)) )
+                             nodes=[kayak.Inputs(bond_features(bond)[None, :])] ))
 
     return graph
 
@@ -132,11 +152,13 @@ def main():
     graph = BuildGraphFromMolecule(mol)
 
     # Build a Kayak neural net from that molecule
-    #net, weights = BuildNetFromGraph(graph)
+    net, weights = BuildNetFromGraph(graph)
 
     # Test network
-    #print net.value(True)
-    #print net.checkgrad()
+    for jj, wt in enumerate(weights):
+        diff = kayak.util.checkgrad(wt, net, 1e-4)
+        print diff
+        assert diff < 1e-4
 
 
 
