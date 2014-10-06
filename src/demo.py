4793800be4e0fc20cb288532bece270e00ee2c67
David Duvenaud <dduvenaud@seas.harvard.edu>
2014-10-06 19:06:43 -0400

    Got first version of neural network visualization working

--- src/demo.py
+++ src/demo.py
@@ -12,12 +12,17 @@ import numpy.random as npr
 import time
 
 from rdkit import Chem
+from rdkit.Chem import Draw
+import rdkit.Chem.AllChem as AllChem
 
 sys.path.append('../../Kayak/')
 import kayak
 
 from MolGraph import *
 from features import *
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def BuildNetFromGraph(graph, num_hidden_features = [5, 6]):
@@ -81,11 +86,17 @@ def BuildGraphFromMolecule(mol):
 
     graph = MolGraph()
 
+    AllChem.Compute2DCoords(mol)    # Only for visualization.
+
+
     # Iterate over the atoms.
     rd_atoms = {}
     for atom in mol.GetAtoms():
         rd_atoms[atom.GetIdx()] = Vertex( nodes = [kayak.Inputs(atom_features(atom)[None,:])] )
-        graph.add_vert( rd_atoms[atom.GetIdx()] )
+        new_vert = rd_atoms[atom.GetIdx()]
+        pos = mol.GetConformer().GetAtomPosition(atom.GetIdx())
+        new_vert.pos = (pos.x, pos.y)
+        graph.add_vert( new_vert )
 
     # Iterate over the bonds.
     for bond in mol.GetBonds():
@@ -99,12 +110,82 @@ def BuildGraphFromMolecule(mol):
     return graph
 
 
+# Define the positions of a node.
+def position(k_node, node_positions):
+    """node is a kayak node."""
+    if k_node in node_positions:
+        return node_positions[k_node]
+    else:
+        parents_positions = [position(p, node_positions)
+                             for p in k_node._parents
+                             if not isinstance(p, (kayak.Parameter, kayak.Targets))]
+        (xs, ys, zs) = zip(*parents_positions)
+        new_x = np.mean(xs)
+        new_y = np.mean(ys)
+        new_z = np.max(zs) + 1
+        node_positions[k_node] = (new_x, new_y, new_z)
+        return position(k_node, node_positions)
+
+def drawComputationGraph(mol_graph, target, mol):
+    """Graph already contains a list of vertices, each one has a Kayak Input,
+    which knows about its children.
+
+    Graph is the mol graph.
+    """
+
+    # First, ask RDKit to draw the molecule (doesn't work yet)
+    # For now, just do a crude drawing by us.
+    ax = plt.figure().add_subplot(111, projection = '3d')
+    for e in mol_graph.get_edges():
+        (v1, v2) = e.get_verts()
+        ax.plot([v1.pos[0], v2.pos[0]], [v1.pos[1], v2.pos[1]], [0., 0])
+
+    # Make a dict indexed by kayak nodes that has the location of each atom as values.
+    node_positions = {}
+    base_nodes = mol_graph.get_verts()
+    for k_n, mol_v in [(mol_v.nodes[0], mol_v) for mol_v in base_nodes]:
+        node_positions[k_n] = mol_v.pos + (0,)
+
+    base_edges = mol_graph.get_edges()
+    for mol_e in base_edges:
+        mol_v1, mol_v2 = mol_e.get_verts()
+        avg_x = (mol_v1.pos[0] + mol_v2.pos[0]) / 2.0
+        avg_y = (mol_v1.pos[1] + mol_v2.pos[1]) / 2.0
+        avg_z = 0
+        node_positions[mol_e.nodes[0]] = (avg_x, avg_y, avg_z)
+
+    for n1, n2 in find_all_edges_leading_to(target):
+        if not isinstance(n1, (kayak.Parameter, kayak.Targets)) and not isinstance(n2, (kayak.Parameter, kayak.Targets)):
+            pos1 = position(n1, node_positions)
+            pos2 = position(n2, node_positions)
+            ax.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], [pos1[2], pos2[2]])
+            #ax.plot(zip(*(pos1, pos2)))
+
+    plt.show()
+
+
+def find_all_edges_leading_to(k_target):
+    # TODO: Move into Kayak.
+    found = {}
+    find_all_edges_internal(k_target, found)
+    return found
+
+def find_all_edges_internal(k_node, found):
+    # Works backwards to find all edges of parents leading to the current node.
+    for p in k_node._parents:
+        if (p, k_node) not in found:
+            find_all_edges_internal(p, found)
+        found[(p, k_node)] = None
+    return
+
+
 def main():
 
     # Load in a molecule
     print "Parsing the molecule..."
+    #mol = Chem.MolFromSmiles('CC')   # Caffeine
     mol = Chem.MolFromSmiles('CN1C=NC2=C1C(=O)N(C(=O)N2C)C')   # Caffeine
-    mol = Chem.MolFromSmiles('CCN(CC)C(=O)[C@H]1CN([C@@H]2Cc3c[nH]c4c3c(ccc4)C2=C1)C')  # LSD
+    #mol = Chem.MolFromSmiles('CCN(CC)C(=O)[C@H]1CN([C@@H]2Cc3c[nH]c4c3c(ccc4)C2=C1)C')  # LSD
 
     # Build a graph from it
     print "Building the graph of the molecule..."
@@ -113,7 +194,7 @@ def main():
 
     # Build a Kayak neural net from that molecule
     print "Building the custom neural net..."
-    net, weights = BuildNetFromGraph(graph, num_hidden_features = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
+    net, weights = BuildNetFromGraph(graph, num_hidden_features = [2, 2, 2])
     print "Time elapsed:", time.clock() - start
 
     print "Evaluating the network..."
@@ -127,12 +208,24 @@ def main():
     print "Time elapsed:", time.clock() - start
 
     # Test network
-    print "Checking gradients..."
-    print kayak.util.checkgrad(weights[0], net, 1e-4)
-    for jj, wt in enumerate(weights):
-        diff = kayak.util.checkgrad(wt, net, 1e-4)
-        print diff
-        assert diff < 1e-4
+    #print "Checking gradients..."
+    #print kayak.util.checkgrad(weights[0], net, 1e-4)
+    #for jj, wt in enumerate(weights):
+    #    diff = kayak.util.checkgrad(wt, net, 1e-4)
+    #    print diff
+    #    assert diff < 1e-4
+
+    # Visualize the resulting neural net
+    #fig = Draw.MolToMPL(mol)
+    #fig.show()
+    #Draw.MolToFile(mol, 'molecule-image.png', size=(1000,1000))
+
+    # m = Chem.MolFromSmiles('c1ccccc1')
+    # AllChem.Compute2DCoords(m)
+    # pos = m.GetConformer().GetAtomPosition(0)
+
+    drawComputationGraph(graph, net, mol)
+    print "Done"
 
 
 
