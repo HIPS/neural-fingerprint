cabf334583dbf01e30fb80176f2fec5e83b670f6
David Duvenaud <dduvenaud@seas.harvard.edu>
2014-10-08 19:53:16 -0400

    Historic thing:  Learning on molecules

--- src/compare-fingerprints-to-convnet.py
+++ src/compare-fingerprints-to-convnet.py
@@ -50,7 +50,7 @@ def train_2layer_nn(features, targets):
 
     W1 = kayak.Parameter( 0.1*npr.randn( features.shape[1], h1_size ))
     B1 = kayak.Parameter( 0.1*npr.randn( 1, h1_size ) )
-    H1 = kayak.Dropout(kayak.HardReLU(kayak.ElemAdd(kayak.MatMult(X, W1), B1)), h1_dropout)
+    H1 = kayak.Dropout(kayak.HardReLU(kayak.ElemAdd(kayak.MatMult(X, W1), B1)), h1_dropout, batcher=batcher)
 
     W2 = kayak.Parameter( 0.1*npr.randn( h1_size, 1 ) )
     B2 = kayak.Parameter( 0.1*npr.randn(1,1))
@@ -92,8 +92,8 @@ def train_2layer_nn(features, targets):
         print epoch, total_err / total_data
 
     def make_predictions(newvals):
-        X.value = newvals
-        H1.reinstate_units()
+        X.data = newvals
+        batcher.test_mode()
         return Y.value*targ_std + targ_mean
 
     return make_predictions
@@ -111,8 +111,9 @@ def load_molecules(filename, test=False, predict_field='rate', transform=None, c
     fingerprints = [smile_to_fp(s) for s in list(data['smiles'])]
 
     alldata = {}
-    alldata['x'] = stringlist2intarray(fingerprints)  # N by D
+    alldata['fingerprints'] = stringlist2intarray(fingerprints)  # N by D
     alldata['inchi_key'] = data['inchi_key']
+    alldata['smiles'] = data['smiles']
 
     if test == False:
         alldata['y'] = np.array(transform(data[predict_field]))[:, None]  # Load targets
@@ -140,66 +141,6 @@ def smile_to_fp(s):
     m = Chem.MolFromSmiles(s)
     return (AllChem.GetMorganFingerprintAsBitVect(m, radius,
                                                   nBits=fplength)).ToBitString()
-
-
-
-
-def BuildNetFromGraph(graph, num_hidden_features = [5, 6]):
-
-    # This first version just tries to emulate ECFP.
-    # Different weights on each layer
-
-    num_layers = len(num_hidden_features)
-    num_atom_features = graph.verts[0].nodes[0].shape[1]
-    num_edge_features = graph.edges[0].nodes[0].shape[1]
-
-    # Every atom and edge is a separate Kayak Input.
-    # These inputs already live in the graph.
-
-    W_self = []
-    W_other = []
-    W_edge = []
-
-    for layer in range(num_layers):
-
-        num_prev_layer_features = graph.verts[0].nodes[layer].shape[1]
-
-        # Create a Kayak parameter for this layer
-        W_self.append(kayak.Parameter(0.1*npr.randn(num_prev_layer_features, num_hidden_features[layer])))
-        # possible refinement: separate weights for each connection, max-pooled over all permutations
-        W_other.append(kayak.Parameter(0.1*npr.randn(num_prev_layer_features, num_hidden_features[layer])))
-        W_edge.append(kayak.Parameter(0.1*npr.randn(num_edge_features, num_hidden_features[layer])))
-
-        for v in graph.verts:
-            # Create a Differentiable node N that depends on the corresponding node in the previous layer, its edges,
-            # and its neighbours.
-            mults = [kayak.MatMult(v.nodes[layer], W_self[layer])]
-            for e in v.edges:
-                mults.append(kayak.MatMult( e.nodes[layer], W_edge[layer]))
-            for n in v.get_neighbors()[0]:
-                mults.append(kayak.MatMult( n.nodes[layer], W_other[layer]))
-
-            # Add the next layer of computation to this node.
-            v.nodes.append(kayak.SoftReLU(kayak.ElemAdd(*mults)))
-
-        for e in graph.edges:
-            e.nodes.append(kayak.Identity(e.nodes[layer]))
-
-    # Connect everything to the fixed-size layer using some sort of max
-    penultimate_nodes = [v.nodes[-1] for v in graph.verts]
-    concatenated = kayak.Concatenate( 0, *penultimate_nodes)
-    output_layer = kayak.MatSum( concatenated, 0)
-
-    # Perform a little more computation to get a single number.
-    W_out = kayak.Parameter(0.1*npr.randn(num_hidden_features[-1], 1))
-    output = kayak.MatMult(output_layer, W_out)
-
-    target = kayak.Targets(np.array([[1.23]]));
-    loss = kayak.L2Loss( output, target)
-
-    weights = W_self + W_other + W_edge + [W_out]
-
-    return loss, weights
 
 
 
@@ -233,112 +174,132 @@ def BuildGraphFromMolecule(mol):
 
 
 
-def netFromSmiles(smiles, weights):
-    mol = Chem.MolFromSmiles(smiles)
-    graph = BuildGraphFromMolecule(mol, weights)
-    return BuildNetFromGraph(graph, num_hidden_features = [2, 2, 2])
 
+def BuildNetFromGraph(graph, np_weights, target, num_layers):
+    # This first version just tries to emulate ECFP, with different weights on each layer
 
-def train_custom_nn(smiles, targets):
+    # Dict comprehension to loop over layers and types.
+    k_weights = {key: kayak.Parameter(weights) for key, weights in np_weights.iteritems()}
+
+    for layer in range(num_layers):
+        # Every atom and edge is a separate Kayak Input. These inputs already live in the graph.
+        for v in graph.verts:
+            # Create a Differentiable node N that depends on the corresponding node in the previous layer, its edges,
+            # and its neighbours.
+            mults = [kayak.MatMult(v.nodes[layer], k_weights[('self', layer)])]
+            for n in v.get_neighbors()[0]:
+                mults.append(kayak.MatMult( n.nodes[layer], k_weights[('other', layer)]))
+            for e in v.edges:
+                mults.append(kayak.MatMult( e.nodes[layer], k_weights[('edge', layer)]))
+
+            # Add the next layer of computation to this node.
+            v.nodes.append(kayak.SoftReLU(kayak.ElemAdd(*mults)))
+
+        for e in graph.edges:
+            e.nodes.append(kayak.Identity(e.nodes[layer]))
+
+    # Connect everything to the fixed-size layer using some sort of max
+    penultimate_nodes = [v.nodes[-1] for v in graph.verts]
+    concatenated = kayak.Concatenate( 0, *penultimate_nodes)
+    output_layer = kayak.MatSum( concatenated, 0)   # TODO: Turn sum into a softmax.
+
+    # Perform a little more computation to get a single number.
+    output = kayak.MatMult(output_layer, k_weights['out'])
+
+    return kayak.L2Loss(output, kayak.Targets(target)), k_weights, output
+
+def train_custom_nn(smiles, targets, num_hidden_features = [10, 10]):
+
+    num_layers = len(num_hidden_features)
+
+    # Figure out how many features we have.
+    graph = BuildGraphFromMolecule(Chem.MolFromSmiles('C=C'))   # Hacky? You decide.
+    num_atom_features = graph.verts[0].nodes[0].shape[1]
+    num_edge_features = graph.edges[0].nodes[0].shape[1]
+    num_features = [num_atom_features] + num_hidden_features
+
+    # Create weights ot be passed in to all the networks we build.
+    np_weights = {}
+    for layer in range(num_layers):
+        np_weights[('self', layer)] = 0.1*npr.randn(num_features[layer], num_features[layer + 1])
+        np_weights[('other', layer)] = 0.1*npr.randn(num_features[layer], num_features[layer + 1])
+        np_weights[('edge', layer)] = 0.1*npr.randn(num_edge_features, num_features[layer + 1])
+
+    np_weights['out'] = 0.1*npr.randn(num_features[-1], 1)
 
     # Normalize the outputs.
     targ_mean = np.mean(targets)
     targ_std  = np.std(targets)
 
-    batcher = kayak.Batcher(batch_size, features.shape[0])
+    # Build a list of custom neural nets, one for each molecule, all sharing the same set of weights.
+    losses = []
+    all_k_weights = []
+    print "Building molecular nets",
+    for smile, target in zip(smiles, targets):
+        mol = Chem.MolFromSmiles(smile)
+        graph = BuildGraphFromMolecule(mol)
+        loss, k_weights, _ = BuildNetFromGraph(graph, np_weights, (target - targ_mean)/targ_std, num_layers)
+        losses.append(loss)
+        all_k_weights.append(k_weights)
+        print ".",
 
-    net, weights = netFromSmiles(smiles)
+    # Now actually learn.
 
-    X = kayak.Inputs(features, batcher)
-    T = kayak.Targets((targets[:,None] - targ_mean) / targ_std, batcher)
+    learn_rate = 1e-6
 
-    W1 = kayak.Parameter( 0.1*npr.randn( features.shape[1], h1_size ))
-    B1 = kayak.Parameter( 0.1*npr.randn( 1, h1_size ) )
-    H1 = kayak.Dropout(kayak.HardReLU(kayak.ElemAdd(kayak.MatMult(X, W1), B1)), h1_dropout)
-
-    W2 = kayak.Parameter( 0.1*npr.randn( h1_size, 1 ) )
-    B2 = kayak.Parameter( 0.1*npr.randn(1,1))
-
-    Y = kayak.ElemAdd(kayak.MatMult(H1, W2), B2)
-
-    L = kayak.MatSum(kayak.L2Loss(Y, T))
-
-    mom_grad_W1 = np.zeros(.shape)
-    mom_grad_W2 = np.zeros(W2.shape)
-
+    # TODO: implement RMSProp or whatever.
+    print "\nTraining parameters",
+    num_epochs = 100
     for epoch in xrange(num_epochs):
+        total_loss = 0
+        for loss, k_weights in zip(losses, all_k_weights):
+            # Loop over kayak parameter vectors and evaluate the gradient w.r.t. each one.
+            # Take a step on all parameters.
+            for key, cur_k_weights in k_weights.iteritems():
+                # Set weights value to None so that the weights arrays can be garbage-collected
+                cur_k_weights.value = np_weights[key]
+            total_loss += loss.value
+            for key, cur_k_weights in k_weights.iteritems():
+                cur_grad = loss.grad(cur_k_weights)
+                np_weights[key] -= cur_grad * learn_rate
+            for key, cur_k_weights in k_weights.iteritems():
+                # Set weights value to None so that the weights arrays can be garbage-collected
+                cur_k_weights.value = None
+        print "Current loss after epoch", epoch, ":", total_loss
 
-        total_loss = 0.0
-        total_err  = 0.0
-        total_data = 0
-
-        for batch in batcher:
-
-            H1.draw_new_mask()
-
-            total_loss += L.value
-            total_err  += np.sum(np.abs(Y.value - T.value))
-            total_data += T.shape[0]
-
-            grad_W1 = L.grad(W1)
-            grad_B1 = L.grad(B1)
-            grad_W2 = L.grad(W2)
-            grad_B2 = L.grad(B2)
-
-            mom_grad_W1 = momentum*mom_grad_W1 + (1.0-momentum)*grad_W1
-            mom_grad_W2 = momentum*mom_grad_W2 + (1.0-momentum)*grad_W2
-
-            W1.value += -learn_rate * mom_grad_W1
-            W2.value += -learn_rate * mom_grad_W2
-            B1.value += -learn_rate * grad_B1
-            B2.value += -learn_rate * grad_B2
-
-        print epoch, total_err / total_data
-
-    def make_predictions(newvals):
-        X.value = newvals
-        H1.reinstate_units()
-        return Y.value*targ_std + targ_mean
+    def make_predictions(smile):
+        mol = Chem.MolFromSmiles(smile)
+        graph = BuildGraphFromMolecule(mol)
+        _, _, output = BuildNetFromGraph(graph, np_weights, None)
+        return output.value*targ_std + targ_mean
 
     return make_predictions
-
 
 
 def main():
 
     datadir = '/Users/dkd/Dropbox/Molecule_ML/data/Samsung_September_8_2014/'
 
-    trainfile = datadir + 'davids-validation-split/train_split.csv'
+    #trainfile = datadir + 'davids-validation-split/train_split.csv'
+    trainfile = datadir + 'davids-validation-split/tiny.csv'
     testfile = datadir + 'davids-validation-split/test_split.csv'
 
     print "Loading training data..."
     traindata = load_molecules(trainfile, transform = np.log)
-    features = traindata['x']
-    targets = traindata['y'][:,0]
 
-    CV = kayak.CrossValidator(num_folds, features, targets)
+    # Custom Neural Net
+    pred_func_custom = train_custom_nn(traindata['smiles'], traindata['y'])
+    train_preds = pred_func_custom( train_features )
+    valid_preds = pred_func_custom( valid_features )
+    print "Custom net performance: ", \
+        np.mean(np.abs(train_preds-train_targets)), np.mean(np.abs(valid_preds-valid_targets))
 
-    for ii, fold in enumerate(CV):
-        print "Fold %d" % (ii+1)
-
-        train_features, train_targets = fold.train()
-        valid_features, valid_targets = fold.valid()
-
-        # Custom Neural Net
-        pred_func_custom = train_custom_nn(train_features, train_targets)
-        train_preds = pred_func_custom( train_features )
-        valid_preds = pred_func_custom( valid_features )
-        print "Custom net performance: ", \
-            np.mean(np.abs(train_preds-train_targets)), np.mean(np.abs(valid_preds-valid_targets))
-
-
-
-        # Vanilla Neural Net
-        pred_func_vanilla = train_2layer_nn(train_features, train_targets)
-        train_preds = pred_func_vanilla( train_features )
-        valid_preds = pred_func_vanilla( valid_features )
-        print "Vanilla net performance: ", \
-            np.mean(np.abs(train_preds-train_targets)), np.mean(np.abs(valid_preds-valid_targets))
+    # Vanilla Neural Net
+    pred_func_vanilla = train_2layer_nn(traindata['fingerprints'], traindata['y'])
+    train_preds = pred_func_vanilla( train_features )
+    valid_preds = pred_func_vanilla( valid_features )
+    print "Vanilla net performance: ", \
+        np.mean(np.abs(train_preds-train_targets)), np.mean(np.abs(valid_preds-valid_targets))
         
 
 if __name__ == '__main__':
