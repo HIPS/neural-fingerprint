a49ebbc824077a1aef7f07c910a8f018ebf89ad9
David Duvenaud <dduvenaud@seas.harvard.edu>
2014-11-14 22:13:17 -0500

    Get closer to figure auto-generation

--- paper/make_all_figures.py
+++ paper/make_all_figures.py
@@ -8,11 +8,15 @@ from deepmolecule import plot_predictions, plot_maximizing_inputs, print_weight_
 def main():
     figures_dir = os.path.expanduser("~/repos/DeepMolecules/paper/figures")
     atom2vec_experiment_dir = os.path.expanduser("~/repos/DeepMoleculesData/results/2014-11-14-predict-mass-0")
-    atom2vec_weights_file = os.path.join(atom2vec_experiment_dir, 'convnet-predictions-mass.npz')
+    atom2vec_predictions_file = os.path.join(atom2vec_experiment_dir, 'convnet-predictions-mass.npz')
+    atom2vec_weights_file = os.path.join(atom2vec_experiment_dir, 'conv-net-weights.npz')
 
-    plot_predictions(atom2vec_weights_file, os.path.join(figures_dir, 'convnet-predict-mass-plots'))
+    print "Plotting prediction accuracy..."
+    plot_predictions(atom2vec_predictions_file, os.path.join(figures_dir, 'convnet-predict-mass-plots'))
+    print "Plotting inputs which most maximize features..."
     plot_maximizing_inputs(build_universal_net, atom2vec_weights_file,
                            os.path.join(figures_dir, 'convnet-features-mass'))
+    print "Plotting vector representations..."
     print_weight_meanings(atom2vec_weights_file,
                           os.path.join(figures_dir, 'atom2vec-mass-plots'), 'true-vs-vecs')
 
