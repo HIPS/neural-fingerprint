95d589804a1b46fdb87c46a4b1bddb34f62a5ffd
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-03-19 15:12:28 -0400

    Removed Kayak version of vanilla neural nets

--- deepmolecule/build_vanilla_net.py
+++ deepmolecule/build_vanilla_net.py
@@ -1,70 +1,13 @@
-import kayak as ky
 import numpy as np
-import numpy.random as npr
-from util import WeightsContainer, c_value, c_grad, memoize, WeightsParser
+from util import WeightsContainer, memoize, WeightsParser
 from deepmolecule import smiles_to_fps
 from autograd import grad
-
-def build_vanilla_net(num_inputs, h1_size, h1_dropout):
-    """Just a plain old 2-layer net, nothing to do with molecules."""
-    weights = WeightsContainer()
-    # Need to give fake data here so that the dropout node can make a mask.
-    inputs =  ky.Inputs(np.zeros((1, num_inputs)))
-    W1 = weights.new((num_inputs, h1_size), name='layer 1 weights')
-    B1 = weights.new((1, h1_size), name='layer 1 biases')
-    hidden = ky.HardReLU(ky.MatMult(inputs, W1) + B1)
-    dropout = ky.Dropout(hidden, drop_prob=h1_dropout, rng=npr.RandomState(1))
-    W2 = weights.new(h1_size, name='layer 2 weights')
-    B2 = weights.new(1, name='layer 2 bias')
-    output =  ky.MatMult(dropout, W2) + B2
-    target =  ky.Blank()
-    loss =  ky.L2Loss(output, target)
-
-    # All the functions we'll need to train and predict with this net.
-    def grad_fun(w, X, t):
-        """X is a matrix of size num_training_examples by num_features."""
-        inputs.value = X      # Necessary so that the dropout mask will be the right size.
-        dropout.draw_new_mask()   # TODO: Should be wrapped in a stochastic setup callback.
-        return c_grad(loss, weights, {weights : w, target : t})
-    def loss_fun(w, X, t):
-        return c_value(loss, {weights : w, inputs : X, target : t})
-    def pred_fun(w, X):
-        inputs.value = X      # Necessary so that the dropout mask will be the right size.
-        dropout.reinstate_units()
-        return c_value(output, {weights : w, inputs : X})
-    def hidden_layer_fun(w, X):
-        inputs.value = X      # Necessary so that the dropout mask will be the right size.
-        return c_value(hidden, {weights : w, inputs : X})
-
-    return loss_fun, grad_fun, pred_fun, hidden_layer_fun, weights
-
-
-def build_morgan_deep_net(fp_length=512, fp_radius=4, h1_size=500, h1_dropout=0.1):
-    """A 2-layer net whose inputs are Morgan fingerprints."""
-    v_loss_fun, v_grad_fun, v_pred_fun, v_hiddens_fun, weights = \
-        build_vanilla_net(num_inputs=fp_length, h1_size=h1_size, h1_dropout=h1_dropout)
-
-    def fingerprints_from_smiles(smiles):
-
-        return fingerprints_from_smiles_tuple(tuple(smiles))
-
-    @memoize # This wrapper function exists because tuples can be hashed, but arrays can't.
-    def fingerprints_from_smiles_tuple(smiles_tuple):
-        return smiles_to_fps(smiles_tuple, fp_length, fp_radius)
-
-    def grad_fun(w, s, t): return v_grad_fun(w, fingerprints_from_smiles(s), t)
-    def loss_fun(w, s, t): return v_loss_fun(w, fingerprints_from_smiles(s), t)
-    def pred_fun(w, s):    return v_pred_fun(w, fingerprints_from_smiles(s))
-    def hiddens_fun(w, s): return v_hiddens_fun(w, fingerprints_from_smiles(s))
-
-    return loss_fun, grad_fun, pred_fun, hiddens_fun, weights
-
 
 def relu(X):
     "Rectified linear activation function."
     return X * (X > 0)
 
-def build_vanilla_net_ag(num_inputs, h1_size):
+def build_vanilla_net(num_inputs, h1_size):
     """Just a plain old 2-layer net, nothing to do with molecules."""
     parser = WeightsParser()
     parser.add_weights('layer 1 weights', (num_inputs, h1_size))
@@ -87,11 +30,10 @@ def build_vanilla_net_ag(num_inputs, h1_size):
         return np.sum((preds - targets)**2)
     return loss_fun, grad(loss_fun), pred_fun, hiddens, parser
 
-
-def build_morgan_deep_net_ag(fp_length=512, fp_radius=4, h1_size=500):
+def build_morgan_deep_net(fp_length=512, fp_radius=4, h1_size=500):
     """A 2-layer net whose inputs are Morgan fingerprints."""
     v_loss_fun, v_grad_fun, v_pred_fun, v_hiddens_fun, parser = \
-        build_vanilla_net_ag(num_inputs=fp_length, h1_size=h1_size)
+        build_vanilla_net(num_inputs=fp_length, h1_size=h1_size)
 
     def fingerprints_from_smiles(smiles):
         return fingerprints_from_smiles_tuple(tuple(smiles))
@@ -106,7 +48,6 @@ def build_morgan_deep_net_ag(fp_length=512, fp_radius=4, h1_size=500):
     def hiddens_fun(w, s): return v_hiddens_fun(w, fingerprints_from_smiles(s))
 
     return loss_fun, grad_fun, pred_fun, hiddens_fun, parser
-
 
 def build_morgan_flat_net(fp_length=512, fp_radius=4):
     """Wraps functions for computing Morgan fingerprints."""
