5e0e4cb7b3010d176cf66efd7553ebce9d32acac
Dougal <d.maclaurin@gmail.com>
2015-05-14 15:19:01 -0400

    Removed batch normalization from canonicalization pass

--- deepmolecule/build_convnet.py
+++ deepmolecule/build_convnet.py
@@ -9,6 +9,8 @@ from build_vanilla_net import build_fingerprint_deep_net
 
 from autograd.scipy.misc import logsumexp
 
+seed = 3
+
 def fast_array_from_list(xs):
     return np.concatenate([np.expand_dims(x, axis=0) for x in xs], axis=0)
 
@@ -20,17 +22,9 @@ def softened_max(X, axis=0):
     exp_X = np.exp(X)
     return np.sum(exp_X * X, axis) / np.sum(exp_X, axis)
 
-def weighted_softened_max(X, axis=0):
-    # X is (permutations x atoms x features).
-    # The first feature softly scales how much each
-    # permutation is used to weight the final activations.
-    scale_feature = X[:, :, 0:1]
-    # logsumexp with keepdims requires scipy >= 0.15.
-    return np.sum(X * np.exp(scale_feature - logsumexp(scale_feature, axis, keepdims=True)), axis)
-
-def matmult_neighbors(mol_nodes, other_ntypes, feature_sets, get_weights):
+def matmult_neighbors(array_rep, other_ntypes, feature_sets, get_weights):
     def neighbor_list(degree, other_ntype):
-        return mol_nodes[(other_ntype + '_neighbors', degree)]
+        return array_rep[(other_ntype + '_neighbors', degree)]
     activations_by_degree = []
     for degree in [1, 2, 3, 4]:
         # TODO: make this (plus the stacking) into an autograd primitive
@@ -52,7 +46,8 @@ def weights_name(layer, degree):
     return "layer " + str(layer) + " degree " + str(degree) + " filter"
 
 def build_convnet_fingerprint_fun(atom_vec_dim=20, bond_vec_dim=10,
-                                  num_hidden_features=[100, 100], fp_length=512):
+                                  num_hidden_features=[100, 100], fp_length=512,
+                                  FLIP=False):
     """Sets up functions to compute convnets over all molecules in a minibatch together.
        The number of hidden layers is the length of num_hidden_features - 1."""
 
@@ -60,8 +55,8 @@ def build_convnet_fingerprint_fun(atom_vec_dim=20, bond_vec_dim=10,
     parser = WeightsParser()
     parser.add_weights('atom2vec', (num_atom_features(), atom_vec_dim))
     parser.add_weights('bond2vec', (num_bond_features(), bond_vec_dim))
-
-    in_and_out_sizes = zip([atom_vec_dim] + num_hidden_features[:-1], num_hidden_features)
+    all_layer_sizes = [atom_vec_dim] + num_hidden_features
+    in_and_out_sizes = zip(all_layer_sizes[:-1], all_layer_sizes[1:])
     for layer, (N_prev, N_cur) in enumerate(in_and_out_sizes):
         parser.add_weights("layer " + str(layer) + " biases", (1, N_cur))
         parser.add_weights("layer " + str(layer) + " self filter", (N_prev, N_cur))
@@ -69,35 +64,51 @@ def build_convnet_fingerprint_fun(atom_vec_dim=20, bond_vec_dim=10,
         for degree in [1, 2, 3, 4]:
             parser.add_weights(weights_name(layer, degree), (degree,) + base_shape)
 
-    parser.add_weights('final layer weights', (num_hidden_features[-1], fp_length))
+    parser.add_weights('final layer weights', (all_layer_sizes[-1], fp_length))
     parser.add_weights('final layer bias', (1,fp_length))
 
+    def update_layer(weights, layer, atom_features, bond_features, array_rep, normalize=False):
+        def get_weights_func(degree):
+            return parser.get(weights, weights_name(layer, degree))
+        layer_bias = parser.get(weights, "layer " + str(layer) + " biases")
+        layer_self_weights = parser.get(weights, "layer " + str(layer) + " self filter")
+        self_activations = np.dot(atom_features, layer_self_weights)
+        neighbour_activations = matmult_neighbors(array_rep, ('atom', 'bond'),
+            (atom_features, bond_features), get_weights_func)
+
+        total_activations = neighbour_activations + self_activations
+        if normalize:
+            total_activations = batch_normalize(total_activations)
+
+        return np.tanh(total_activations + layer_bias)
+
     def last_layer_features(weights, array_rep):
-        atom_features = np.dot(array_rep['atom_features'], parser.get(sorting_weights, 'atom2vec'))
-        bond_features = np.dot(array_rep['bond_features'], parser.get(sorting_weights, 'bond2vec'))
+        atom_features = np.dot(array_rep['atom_features'], parser.get(weights, 'atom2vec'))
+        bond_features = np.dot(array_rep['bond_features'], parser.get(weights, 'bond2vec'))
 
         for layer in xrange(len(num_hidden_features)):
-            def get_weights_func(degree):
-                return parser.get(weights, weights_name(layer, degree))
-            layer_bias = parser.get(weights, "layer " + str(layer) + " biases")
-            layer_self_weights = parser.get(weights, "layer " + str(layer) + " self filter")
-            self_activations = np.dot(atom_features, layer_self_weights)
-            neighbour_activations = matmult_neighbors(array_rep, ('atom', 'bond'),
-                (atom_features, bond_features), get_weights_func)
-            total_activations = batch_normalize(neighbour_activations + self_activations)
-            atom_features = np.tanh(total_activations + layer_bias)
+            atom_features = update_layer(weights, layer, atom_features, bond_features, array_rep,
+                                         normalize=True)
         return atom_features
 
     # Generate random weights for sorting.
-    rs = npr.RandomState(0)
-    sorting_weights = rs.rand(len(parser))
-
+    rs = npr.RandomState(seed)
+    sorting_weights = 0.01 * rs.randn(len(parser))
     def canonicalizer(array_rep):
-        # Sorts lists of atoms into a canonical.
-        atom_features = last_layer_features(sorting_weights, array_rep)
+        # Sorts lists of atoms into a canonical ordering.
+        atom_features = np.dot(array_rep['atom_features'], parser.get(sorting_weights, 'atom2vec'))
+        bond_features = np.dot(array_rep['bond_features'], parser.get(sorting_weights, 'bond2vec'))
+        for layer in xrange(len(num_hidden_features)):
+            array_rep = sort_array_rep(array_rep, atom_features, bond_features)
+            atom_features = update_layer(sorting_weights, layer,
+                                         atom_features, bond_features, array_rep,
+                                         normalize=False)
+        array_rep = sort_array_rep(array_rep, atom_features, bond_features)
+        return array_rep
 
+    def sort_array_rep(array_rep, atom_features, bond_features):
         def sort_atoms(atom_idxs, bond_idxs):
-            sort_perm = np.argsort(atom_features[atom_idxs,0])
+            sort_perm = np.argsort(atom_features[atom_idxs, 0] + bond_features[bond_idxs, 0])
             return atom_idxs[sort_perm], bond_idxs[sort_perm]
 
         sorted_array_rep = copy(array_rep)
@@ -111,6 +122,7 @@ def build_convnet_fingerprint_fun(atom_vec_dim=20, bond_vec_dim=10,
                 sorted_atoms, sorted_bonds = sort_atoms(atom_idxs, bond_idxs)
                 sorted_array_rep[('atom_neighbors', degree)][i, :] = sorted_atoms
                 sorted_array_rep[('bond_neighbors', degree)][i, :] = sorted_bonds
+
         return sorted_array_rep
 
     def output_layer_fun(weights, smiles):
@@ -128,7 +140,7 @@ def build_convnet_fingerprint_fun(atom_vec_dim=20, bond_vec_dim=10,
         atom_idxs = array_rep['atom_list']
         return apply_and_stack(atom_idxs, atom_features, softened_max)
 
-    @memoize
+    # @memoize
     def array_rep_from_smiles(smiles):
         """Precompute everything we need from MolGraph so that we can free the memory asap."""
         molgraph = graph_from_smiles_tuple(smiles)
@@ -139,16 +151,24 @@ def build_convnet_fingerprint_fun(atom_vec_dim=20, bond_vec_dim=10,
         for degree in [1, 2, 3, 4]:
             # Since we know the number of neighbors, we can cast to arrays here
             # instead of using lists of lists.
-            arrayrep[('atom_neighbors', degree)] = \
-                    np.array(molgraph.neighbor_list(('atom', degree), 'atom'), dtype=int)
-            arrayrep[('bond_neighbors', degree)] = \
-                    np.array(molgraph.neighbor_list(('atom', degree), 'bond'), dtype=int)
+            if FLIP:
+                arrayrep[('atom_neighbors', degree)] = \
+                        np.array(molgraph.neighbor_list(('atom', degree), 'atom'), dtype=int)[:, ::-1]
+                arrayrep[('bond_neighbors', degree)] = \
+                        np.array(molgraph.neighbor_list(('atom', degree), 'bond'), dtype=int)[:, ::-1]
+            else:
+                print "Original order"
+                arrayrep[('atom_neighbors', degree)] = \
+                        np.array(molgraph.neighbor_list(('atom', degree), 'atom'), dtype=int)
+                arrayrep[('bond_neighbors', degree)] = \
+                        np.array(molgraph.neighbor_list(('atom', degree), 'bond'), dtype=int)
+
         return canonicalizer(arrayrep)
 
     return output_layer_fun, parser
 
 def batch_normalize(activations):
-    return activations / (0.5 * np.std(activations))
+    return activations / (0.5 * np.std(activations, axis=0, keepdims=True))
 
 def build_conv_deep_net(layer_sizes, conv_params):
     # Returns (loss_fun(fp_weights, nn_weights, smiles, targets), pred_fun, net_parser, conv_parser)
